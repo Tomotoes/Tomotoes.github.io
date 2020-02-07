08527d20f877bc08780be0acb1415eef6ae0ebf4
SimonAking <simon@tomotoes.com>
2020-02-07 15:28:33 +0800

    update at Fri Feb 07 2020

--- registerSW.js
+++ registerSW.js
@@ -1,18 +1,1 @@
-window.addEventListener('beforeinstallprompt', function (e) {
-	e.userChoice.then(function (choiceResult) {
-		if (choiceResult.outcome === 'dismissed') {
-			console.log('用户取消安装应用')
-		} else {
-			console.log('用户安装了应用')
-		}
-	})
-})
-
-const version = '2.6.0'
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register(`/sw.js?v=${version}`)
-		.catch(function (err) {
-			console.log('ServiceWorker registration failed: ', err)
-		})
-}
+var SWVersion="5.7.0";window.addEventListener("load",function(){"serviceWorker"in navigator&&navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
