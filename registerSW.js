9da0e1298fe32448e7eca40f51aaf217a463f2eb
SimonAking <simon@tomotoes.com>
2020-02-19 23:02:29 +0800

    update at Wed Feb 19 2020

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-var SWVersion="5.38.3";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
+var SWVersion="5.39.3";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
